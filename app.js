ac41a521fa4e93eb75068de68d8c7d3fc18582f6
AEDJ <115160738+EA50@users.noreply.github.com>
2023-05-16 07:33:56 +0000

    added header styles (html/css)

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/views/index.html');
 });
 
 app.listen(3000, () => {
